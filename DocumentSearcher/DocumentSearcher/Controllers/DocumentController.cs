1350c9291f0bc31a09aaccf32a9f459693fd672f
Klimovich Andrei <andrey_klim@bk.ru>
2015-02-06 19:56:36 +0300

    add interface, simple controller and model for document uploading

--- DocumentSearcher/DocumentSearcher/Controllers/DocumentController.cs
+++ DocumentSearcher/DocumentSearcher/Controllers/DocumentController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using DocumentSearcher.Models;
 
 namespace DocumentSearcher.Controllers
 {
@@ -12,6 +13,26 @@ namespace DocumentSearcher.Controllers
         // GET: /Document/
         public ActionResult Index()
         {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Upload()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Upload(DocumentModel document)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(document);
+            }
+
+            byte[] uploadedFile = new byte[document.File.InputStream.Length];
+            document.File.InputStream.Read(uploadedFile, 0, uploadedFile.Length);
+
             return View();
         }
 	}
---
+++ DocumentSearcher/DocumentSearcher/Models/Document.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using MongoDB.Bson;
+
+namespace DocumentSearcher.Models
+{
+    public class DocumentModel
+    {
+        [Required]
+        public HttpPostedFileBase File { get; set; }
+    }
+}
