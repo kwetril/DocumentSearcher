df692d62bf502c95c6debd8123b8fbb418e05202
kwetril <andrey_klim@bk.ru>
2015-02-09 01:04:40 +0300

    add library project for text indexing, processing and searching algorithms

---
+++ DocumentSearcher/SearchCore/TextProcessors/Implementation/TextTokenizer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SearchCore.TextProcessors.Interfaces;
+
+namespace SearchCore.TextProcessors.Implementation
+{
+    public class TextTokenizer : ITokenizer
+    {
+        public string[] SplitToWords(string text)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
